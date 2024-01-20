349c493a5ba149a343467bcedc875b14c9147879
Nicolas Rist <nic615528@gmail.com>
2024-01-19 23:40:03 -0800

    init

---
+++ src/main/java/com/ristn/cpk/CpkApplication.java
@@ -0,0 +1,13 @@
+package com.ristn.cpk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ristn/cpk/CpkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ristn.cpk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
